734d26d87f69836fc5a44bf5234ec1ecdf12fe60
Muskan2001 <98939873+Muskan2001@users.noreply.github.com>
2022-04-08 13:09:33 +0530

    Update java-driver-code.java

--- numbers/java-driver-code.java
+++ numbers/java-driver-code.java
@@ -21,6 +21,31 @@ public class Main {
     static String solve(int n){
       	// Your code goes here
         // n is the given input
+        int r = 1, num, sum = 0;
+        num = n;
+        while (num > 9)
+        {
+            while (num > 0)
+            {
+                r = num % 10;
+                sum = sum + r;
+                num = num / 10;
+            }
+            num = sum;
+            sum = 0;
+        }
+        if (num == 1)
+        {
+            System.out.println(“magic number");
+        }
+        else
+        {
+            System.out.println(“normal number");
+        }
+    }
+}
+      
+      
       
         String result = "Special";
         return result;
